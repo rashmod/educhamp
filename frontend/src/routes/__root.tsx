8e11fa309621fbb57f51cefe23558b0717c5943d
Bhushan <alhatbhushan@gmail.com>
2024-11-16 11:50:11 +0530

    [frontend] chore: added tanstack query

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 import '@/index.css';
 
+const queryClient = new QueryClient();
+
 export const Route = createRootRoute({
   component: () => (
-    <div className="grid min-h-[100dvh] grid-rows-[auto_1fr]">
-      <div className="container">navbar</div>
-      <div className="container mx-auto grid grid-rows-subgrid p-8">
-        <Outlet />
+    <QueryClientProvider client={queryClient}>
+      <div className="grid min-h-[100dvh] grid-rows-[auto_1fr]">
+        <div className="container">navbar</div>
+        <div className="container mx-auto grid grid-rows-subgrid p-8">
+          <Outlet />
+        </div>
+        <TanStackRouterDevtools />
       </div>
-      <TanStackRouterDevtools />
-    </div>
+    </QueryClientProvider>
   ),
 });
